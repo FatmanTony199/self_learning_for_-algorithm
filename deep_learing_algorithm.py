ef7e13eb2ca4c4309e8d1ad8c430d159713b854c
FatmanTony199 <tony1030127@gmail.com>
2025-03-28 13:35:18 +0800

    linear

--- deep_learing_algorithm.py
+++ deep_learing_algorithm.py
@@ -3,16 +3,36 @@ larning_rate = 0.0045
 num_iterations = 5000
 
 def initialize_parameters(layer_dims):
-	
-    np.random.seed(1)
-    parameters = {}
-    layers = len(layer_dims) 
+
+	np.random.seed(1)
+	parameters = {}
+	ayers = len(layer_dims) 
     
-    for layer in range(1,layers):
-        parameters["W"+str(layer)] = np.random.randn(layer_dims[layer], layer_dims[layer-1])
-        parameters["b"+str(layer)] = np.zeros((layer_dims[layer], 1))
+	for layer in range(1,layers):
+		parameters["W"+str(layer)] = np.random.randn(layer_dims[layer], layer_dims[layer-1])
+		parameters["b"+str(layer)] = np.zeros((layer_dims[layer], 1))
         
-        assert(parameters['W' + str(layer)].shape == (layer_dims[layer], layer_dims[layer-1]))
-        assert(parameters['b' + str(layer)].shape == (layer_dims[layer], 1))
-                                              
-    return parameters
+		assert(parameters['W' + str(layer)].shape == (layer_dims[layer], layer_dims[layer-1]))
+		assert(parameters['b' + str(layer)].shape == (layer_dims[layer], 1))
+	                                              
+	return parameters
+
+def linear_forward(A, W, b):
+
+	Z = np.dot(W,A)+b
+	cache = (A,W,b)
+	assert(Z.shape == (W.shape[0], A.shape[1]))
+	return Z, cache
+    
+def linear_activation_forward(A_pre, W, b, activation):
+
+	if activation == "sigmoid":
+		Z, linear_cache = linear_forward(A_pre, W, b)
+		A, activation_cache = sigmoid(Z)
+	elif activation == "relu":
+		Z, linear_cache = linear_forward(A_pre, W, b)
+		A, activation_cache = relu(Z)
+
+	cache = (linear_cache, activation_cache)
+	assert (A.shape == (W.shape[0], A_pre.shape[1]))
+	return A, cache
