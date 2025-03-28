0bbd10cbaf23f8aa7ad5986ed6f1906c2c58c513
FatmanTony199 <tony1030127@gmail.com>
2025-03-28 13:26:25 +0800

    Random numbers

--- deep_learing_algorithm.py
+++ deep_learing_algorithm.py
@@ -1,3 +1,18 @@
 layer_dims = [X_train.shape[0], 4, 2, 1]
 larning_rate = 0.0045
 num_iterations = 5000
+
+def initialize_parameters(layer_dims):
+	
+    np.random.seed(1)
+    parameters = {}
+    layers = len(layer_dims) 
+    
+    for layer in range(1,layers):
+        parameters["W"+str(layer)] = np.random.randn(layer_dims[layer], layer_dims[layer-1])
+        parameters["b"+str(layer)] = np.zeros((layer_dims[layer], 1))
+        
+        assert(parameters['W' + str(layer)].shape == (layer_dims[layer], layer_dims[layer-1]))
+        assert(parameters['b' + str(layer)].shape == (layer_dims[layer], 1))
+                                              
+    return parameters
